031dd1ae04c0409b2c727a6b633b1aad0e826b44
Kuang He <kuang.he@gmail.com>
2022-02-14 20:40:59 -0600

    Use moment.js to calculate relative time

--- modificationtime.js
+++ modificationtime.js
@@ -1,21 +1,3 @@
-function getRelativeTime(seconds) {
-  let result = '';
-
-  if (seconds < 60) {
-    result = 'last minute';
-  } else if (seconds < 3600) {
-    result = 'last hour';
-  } else if (seconds < 3600 * 24) {
-    result = Math.round(seconds / 3600) + ' hours ago';
-  } else if (seconds < 3600 * 24 * 2) {
-    result = 'yesterday';
-  } else {
-    result = Math.round(seconds / (3600 * 24)) + ' days ago';
-  }
-
-  return result;
-}
-
 // Get modification time of the database
 async function getModificationTime() {
   const url = 'https://zerotrac.github.io/leetcode_problem_rating/data.json';
@@ -23,12 +5,9 @@ async function getModificationTime() {
 
   const modificationTime = response.headers.get('Last-Modified');
   const date = new Date(modificationTime);
-  const diffInSeconds = (Date.now() - date.getTime()) / 1000.0;
-
+  const dateString = date.toString().slice(0, 33) + ' (' + moment(date).fromNow() + ')';
   const elem = document.getElementById('dbModificationTime');
-  const text =
-    date.toString().slice(0, 33) + ' (' + getRelativeTime(diffInSeconds) + ')';
-  elem.innerHTML = '<b>Database last modified:</b> ' + text;
+  elem.innerHTML = '<b>Database last modified:</b> ' + dateString;
 }
 
 getModificationTime();
