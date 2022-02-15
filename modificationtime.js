2e6aa94413603a0c783b72b9a2cfd6d4581f2cfc
Kuang He <kuang.he@gmail.com>
2022-02-15 02:29:27 -0600

    Format date a bit better

--- modificationtime.js
+++ modificationtime.js
@@ -1,3 +1,9 @@
+function formatDate(date) {
+  let result = moment(date).format('YYYY/MM/DD HH:mm');
+  const timezone = -date.getTimezoneOffset() / 60;
+  return result + ' GMT' + (timezone < 0 ? timezone : '+' + timezone);
+}
+
 // Get modification time of the database
 async function getModificationTime() {
   const url = 'https://zerotrac.github.io/leetcode_problem_rating/data.json';
@@ -5,7 +11,7 @@ async function getModificationTime() {
 
   const modificationTime = response.headers.get('Last-Modified');
   const date = new Date(modificationTime);
-  const dateString = date.toString().slice(0, 33) + ' (' + moment(date).fromNow() + ')';
+  const dateString = formatDate(date) + ' (' + moment(date).fromNow() + ')';
   const elem = document.getElementById('dbModificationTime');
   elem.innerHTML = '<b>Database last modified:</b> ' + dateString;
 }
